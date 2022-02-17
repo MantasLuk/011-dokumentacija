1b723344e0aa1e7970f7442ad82f5b4e30c0e329
Mantas Luk <mantasluk7@gmail.com>
2022-02-17 21:15:22 +0200

    Small fixes

--- car.js
+++ car.js
@@ -1,18 +1,18 @@
 /**
- * 
+ * Automobilio kilometrazas pagal sanaudas
  * @param {string} auto Automobilio marke
  * @param {number} tank Bako turis
- * @param {number} highway Magistrales kuro sanaudos /100km
- * @param {number} average Vidutines kuro sanaudos /100km
- * @param {number} city Miesto kuro sanaudos /100km
- * @returns {string} Sakinys apie automobilio kilometraza pagal sanaudas
+ * @param {number} highway Magistrales kuro sanaudos 
+ * @param {number} average Vidutines kuro sanaudos 
+ * @param {number} city Miesto kuro sanaudos
+ * @returns {string} Sakinys
  */
 
 
 function car(auto, tank, highway, average, city) {
-    const highwayKm = (tank/highway *100).toFixed();
-    const cityKm = (tank/city *100).toFixed();
-    const averageKm = (tank/average *100).toFixed();
+    const highwayKm = Math.floor(tank/highway *100)
+    const cityKm = Math.floor(tank/city *100)
+    const averageKm = Math.floor(tank/average *100)
 
     return `${auto} su ${tank}l baku, gali nuvaziuoti ${highwayKm}km (magistralej, kai sanaudos yra ${highway}l/100km), ${averageKm}km (vidutiniskai, kai sanaudos yra ${average}l/100km) ir ${cityKm}km (mieste, kai sanaudos yra ${city}l/100km)`;
 }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const car = require("./car.js");
 
+
+
+
 const volvo = car('Volvo XC90', 60, 6, 7, 8);
 
 console.log(volvo);
